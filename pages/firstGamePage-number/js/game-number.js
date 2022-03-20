cdbf844b6b91d475911e75871e78106608f5041d
porfiriy <porf2003@gmail.com>
2022-03-20 15:35:33 +0300

    file: added functionality to the start button

--- pages/firstGamePage-number/js/game-number.js
+++ pages/firstGamePage-number/js/game-number.js
@@ -1,27 +1,6 @@
-"use strict"
 
-let randNum1;
-let randNum2;
-let randNum3;
-let randNum4;
-let randNum5;
-let randNum6;
-let randNum7;
-let randNum8;
-let randNum9;
-let randNum10;
-let randNum11;
-let randNum12;
-let randNum13;
-let randNum14;
-let randNum15;
-let randNum16;
-let randNum17;
-let randNum18;
-let randNum19;
-let randNum20;
-
-let startGameVieweport = document.querySelectorAll('.start - viewport')
-
-console.log('startGameVieweport');
-
+$(document).ready(function () {
+   $('.number-game-start').click(function (event) {
+      $('.number-game-start,.start-viewport').toggleClass('activated');
+   });
+});
