b7ecbf08294c5d124d86ed6ecbf9b8cd865b5264
porfiriy <porf2003@gmail.com>
2022-04-03 10:47:04 +0300

    file: added button for words-game page

---
+++ pages/secondGameWords/js/game-words.js
@@ -0,0 +1,8 @@
+"use strict"
+
+
+
+document.querySelector('.words-game-start').onclick = function () {
+   document.querySelector('.words-game-start,.start-menu').classList.add('activated');
+   document.querySelector('.words-game-start').classList.add('activated');
+}
