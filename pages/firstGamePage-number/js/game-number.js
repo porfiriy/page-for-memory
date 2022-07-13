64723f59875cce8b3d4404f0f769060113435c6c
porfiriy <porf2003@gmail.com>
2022-07-13 17:15:58 +0300

    file: added new anim text in the num-game

--- pages/firstGamePage-number/js/game-number.js
+++ pages/firstGamePage-number/js/game-number.js
@@ -1,4 +1,9 @@
 'use strict'
+let audioStart = new Audio('/page-for-memory/sound/start-game.mp3');
+let startRandNumbers = document.querySelector('.random-num-container__number');
+let startText = document.querySelector('.random-num-container__text');
+
+
 /*
 let audioClick = new Audio('/page-for-memory/sound/button-sound.mp3');
 
@@ -7,5 +12,19 @@ BUTTON.onclick = function () {
    BUTTON.classList.add('activated');
    if (BUTTON.classList.contains('activated')) {
       audioClick.play();
+   }
+}
+*/
+
+
+//активация кнопки старт при нажатии
+const BUTTON_START = document.querySelector('.button-start');
+BUTTON_START.onclick = function () {
+   document.querySelector('.start-menu').classList.add('activated');
+   BUTTON_START.classList.add('activated');
+   if (BUTTON_START.classList.contains('activated')) {
+      audioStart.play();
+
+      startText.style = 'display: block;';
    }
 }
