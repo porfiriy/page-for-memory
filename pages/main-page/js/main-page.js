6d5b9f8370e0704f996d1fed1bbd219c595a44d6
porfiriy <porf2003@gmail.com>
2022-07-20 13:35:23 +0300

    file: added music in the main menu

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -1,3 +1,13 @@
-'use strict'
 
 
+let audioBackground = new Audio('/page-for-memory/sound/_Ivan M. Lacamar.mp3');
+let audioStart = new Audio('/page-for-memory/sound/click.mp3');
+
+function playAudio() {
+
+
+   audioStart.play();
+   audioBackground.play();
+}
+
+playAudio();
