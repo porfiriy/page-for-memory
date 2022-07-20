b3686b089d64c3c3da2e014780adbaa837b10682
porfiriy <porf2003@gmail.com>
2022-07-20 13:59:35 +0300

    file: deleted music in main menu

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -3,11 +3,3 @@
 let audioBackground = new Audio('/page-for-memory/sound/_Ivan M. Lacamar.mp3');
 let audioStart = new Audio('/page-for-memory/sound/click.mp3');
 
-function playAudio() {
-
-
-   audioStart.play();
-   audioBackground.play();
-}
-
-playAudio();
