7cfc2a41f6fbc88ad61241cc6911bb32c106840d
porfiriy <porf2003@gmail.com>
2022-12-07 23:08:44 +0300

    file(rptn-gm)create new folder "game-repetition"

---
+++ pages/Game-repetition/js/game-repetition.js
@@ -0,0 +1,87 @@
+"use strict"
+
+//звук
+let audioComplete = new Audio('/page-for-memory/sound/successfull2.mp3');
+let audioVictory = new Audio('/page-for-memory/sound/successfull.mp3');
+let audioStart = new Audio('/page-for-memory/sound/start-game.mp3');
+let audioFaile = new Audio('/page-for-memory/sound/faile.mp3');
+let audioClick = new Audio('/page-for-memory/sound/click.mp3');
+
+
+let settings = document.querySelector(".pop-up__container");
+let comeback = document.querySelector(".pop-up__container2");
+let restart = document.querySelector(".pop-up__container3");
+
+//при нажатии на отмену вспл окна настройки 
+document.querySelector('.pop-up__cancel').onclick = function () {
+   settings.style = 'visibility:hidden;';
+
+};
+//при нажатии на иконку настроек
+document.querySelector('.linkToTheSettings').onclick = function () {
+
+   settings.style = 'visibility:visible;';
+
+};
+
+//при нажатии на отмену вспл окна назад
+document.querySelector('.pop-up__cancel2').onclick = function () {
+   comeback.style = 'visibility:hidden;';
+
+};
+//при нажатии на иконку назад
+document.querySelector('.comeback-button').onclick = function () {
+
+   comeback.style = 'visibility:visible;';
+
+};
+
+//при нажатии на отмену вспл окна рестарт
+document.querySelector('.pop-up__cancel3').onclick = function () {
+   restart.style = 'visibility:hidden;';
+
+};
+//при нажатии на иконку рестарт
+document.querySelector('.linkToTheRestart').onclick = function () {
+
+   restart.style = 'visibility:visible;';
+
+};
+
+function game() {
+
+
+
+
+
+   //линия времени 
+   function getId(id) {
+      return document.getElementById(id);
+   }
+
+   startDeadeLine();
+   function startDeadeLine() {
+
+      let deadeLine = getId("deadeLine");
+      deadeLine.style = "animation: deadeLine 70s linear ";
+
+   }
+   //анимация проигриша 
+   function showMessage() {
+      looseTab.style = 'visibility:visible;';
+      audioFaile.play();
+   }
+   deadeLine.addEventListener("animationend", showMessage);
+
+
+   //активация кнопки старт при нажатии
+   const BUTTON_START = document.querySelector('.button-start');
+   BUTTON_START.onclick = function () {
+      document.querySelector('.start-menu').classList.add('activated');
+      BUTTON_START.classList.add('activated');
+      if (BUTTON_START.classList.contains('activated')) {
+         audioStart.play();
+         game();
+      }
+   }
+
