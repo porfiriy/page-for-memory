0859f38fe87b384e36d43560a6be94fd5a1e2027
porfiriy <porf2003@gmail.com>
2023-01-03 20:56:27 +0300

    file(mn-pg): changed navigation menu and padding

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -1,4 +1,13 @@
 'use strict'
+const list = document.querySelectorAll('.list')
+
+function activeLink() {
+   list.forEach((item) =>
+      item.classList.remove('active'));
+   this.classList.add('active');
+}
+list.forEach((item) =>
+   item.addEventListener('click', activeLink));
 
 let iconGameNumbers = document.querySelector('.game-numbers');
 console.log(iconGameNumbers);
