64c7ca69e7e17c1b018f812ec1db697a412f485b
porfiriy <porf2003@gmail.com>
2023-01-04 20:27:34 +0300

    fix(mn-pg) changed menu navigation

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -1,18 +1,19 @@
 'use strict'
 const list = document.querySelectorAll('.list')
+const itemHomeNavigation = document.querySelector('.list-home');
+const itemHomeTextNavigation = document.querySelector('.text-list-item');
 
 function activeLink() {
   list.forEach((item) =>
     item.classList.remove('active'));
+  itemHomeNavigation.classList.remove('list-home');//нужно для того чтобы сбросить стили у элемента home при нажатии любого другого
+  itemHomeTextNavigation.classList.remove('text-active');
   this.classList.add('active');
 }
 list.forEach((item) =>
   item.addEventListener('click', activeLink));
 
-let iconGameNumbers = document.querySelector('.game-numbers');
-console.log(iconGameNumbers);
-iconGameNumbers.addEventListener("mousedown", function () {
-  console.log('JJBH,');
-})
 
-
+itemHomeNavigation.onclick = function () {//при нажатии на ллем home в меню навигац
+  itemHomeTextNavigation.classList.add('text-active');
+}
