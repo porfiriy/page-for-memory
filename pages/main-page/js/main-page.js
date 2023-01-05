35291bda7c4723629fb90f890a3f35390a325bd8
porfiriy <porf2003@gmail.com>
2023-01-05 15:30:57 +0300

    fix(mn-pg) changed menu navigation item-home

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -6,14 +6,15 @@ const itemHomeTextNavigation = document.querySelector('.text-list-item');
 function activeLink() {
   list.forEach((item) =>
     item.classList.remove('active'));
-  itemHomeNavigation.classList.remove('list-home');//нужно для того чтобы сбросить стили у элемента home при нажатии любого другого
-  itemHomeTextNavigation.classList.remove('text-active');
+  itemHomeNavigation.classList.remove('home-active-again');//нужно для того чтобы сбросить стили у элемента home при нажатии любого другого
+  itemHomeTextNavigation.classList.remove('text-active', 'text-active-again');
   this.classList.add('active');
 }
 list.forEach((item) =>
   item.addEventListener('click', activeLink));
 
 
-itemHomeNavigation.onclick = function () {//при нажатии на ллем home в меню навигац
-  itemHomeTextNavigation.classList.add('text-active');
+itemHomeNavigation.onclick = function () {//при нажатии на элем home в меню навигац
+  itemHomeTextNavigation.classList.add('text-active-again');
+  itemHomeNavigation.classList.add('home-active-again');
 }
