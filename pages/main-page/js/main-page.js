ca0a634545b3f79840376b20197391168c8161ed
porfiriy <porf2003@gmail.com>
2023-01-05 16:24:57 +0300

    file(mn-pg)added container achievments

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -3,7 +3,9 @@ const list = document.querySelectorAll('.list')
 const itemHomeNavigation = document.querySelector('.list-home');
 const itemHomeTextNavigation = document.querySelector('.text-list-item');
 const itemGamesNavigation = document.querySelector('.list-games');
+const itemAchievmentsNavigation = document.querySelector('.list-achievements');
 const containerGamesPage = document.querySelector('.page-games-container');
+const containerAchievmentsPage = document.querySelector('.page-achievments-container');
 
 function activeLink() {
   list.forEach((item) =>
@@ -20,8 +22,14 @@ itemHomeNavigation.onclick = function () {//при нажатии на элем 
   itemHomeTextNavigation.classList.add('text-active-again');
   itemHomeNavigation.classList.add('home-active-again');
   containerGamesPage.style = 'display: none;'; //убирает страницу игр
+  containerAchievmentsPage.style = 'display: none;';
 }
 
 itemGamesNavigation.onclick = function () { //при нажатии на кнопку Games
   containerGamesPage.style = 'display: block;';
+  containerAchievmentsPage.style = 'display: none;';//убирает страницу игр
+}
+
+itemAchievmentsNavigation.onclick = function () { //при нажатии на кнопку Achievments
+  containerAchievmentsPage.style = 'display: block;';
 }
