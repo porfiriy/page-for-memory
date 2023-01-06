f4cb5cc363db0a1e2077b2b20670774022bb16f4
porfiriy <porf2003@gmail.com>
2023-01-06 19:45:39 +0300

    file(mn-pg) added new elem. tasks,bonus,iq

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -8,6 +8,8 @@ const itemAchievmentsNavigation = document.querySelector('.list-achievements');
 const containerGamesPage = document.querySelector('.page-games-container');
 const containerAchievmentsPage = document.querySelector('.page-achievments-container');
 const containerStorePage = document.querySelector('.page-store-container');
+const containerEverydayTasks = document.querySelector('.everyday-tasks-container');
+const everydayTasksButton = document.querySelector('.everyday-tasks-button');
 
 function activeLink() {
   list.forEach((item) =>
@@ -26,20 +28,29 @@ itemHomeNavigation.onclick = function () {//при нажатии на элем 
   containerGamesPage.style = 'display: none;'; //убирает страницу игр
   containerAchievmentsPage.style = 'display: none;';
   containerStorePage.style = 'display: none;';
+  containerEverydayTasks.style = 'display: none;';
 }
 
 itemGamesNavigation.onclick = function () { //при нажатии на кнопку Games
   containerGamesPage.style = 'display: block;';
   containerAchievmentsPage.style = 'display: none;';//убирает страницу игр
   containerStorePage.style = 'display: none;';
+  containerEverydayTasks.style = 'display: none;';
 }
 
 itemAchievmentsNavigation.onclick = function () { //при нажатии на кнопку Achievments
   containerAchievmentsPage.style = 'display: block;';
   containerStorePage.style = 'display: none;';
+  containerEverydayTasks.style = 'display: none;';
 }
 
-itemStoreNavigation.onclick = function () { //при нажатии на кнопку Achievments
+itemStoreNavigation.onclick = function () { //при нажатии на кнопку Store
   containerStorePage.style = 'display: block;';
+  containerEverydayTasks.style = 'display: none;';
+}
+
+everydayTasksButton.onclick = function () { //при нажатии на кнопку ежедневки
+  containerEverydayTasks.style = 'display: block;';
+
 
 }
