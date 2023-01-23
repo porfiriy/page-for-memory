ff66daab6ae37c571867dcfd69475379c5cf6673
MooNMisTX <butor.aleksey@mail.ru>
2023-01-23 21:59:04 +0300

    File:(Prjct) fixed comeback in reg-page.Added devs

---
+++ pages/settings-page/settings-page.js
@@ -0,0 +1,15 @@
+'use strict'
+const Devs = document.querySelector('.developers-container');
+const DevsList = document.querySelector('.developers-container_body');
+const containerGrayBackground = document.querySelector('.gray-background-container');
+const DevsClose = document.querySelector('.close');
+
+Devs.onclick = function () { //при нажатии на кнопку
+	DevsList.style = 'display: block;';
+	containerGrayBackground.style = 'display: block;';
+  }
+
+DevsClose.onclick = function () {
+	DevsList.style = 'display: none;';
+	containerGrayBackground.style = 'display: none;';
+  }
