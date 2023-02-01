bbc11cae433e7301581be86aa1d3ec7e2b13f969
MooNMisTX <butor.aleksey@mail.ru>
2023-02-01 20:53:56 +0300

    File:(Menu) Доделаны ежедневки

--- pages/main-page/js/main-page.js
+++ pages/main-page/js/main-page.js
@@ -22,10 +22,16 @@ const wordDayContainer = document.querySelector('.day-word-container');
 const wordDayCloseButton = document.querySelector('.word-day-close-button');
 const wordDayButton = document.querySelector('.everyday-word-button');
 const settingsButton = document.querySelector('.settings')
-
+const everydayWordButton = document.querySelector('.everyday_day-word-container');
+const everydayWord = document.querySelector('.everyday_word');
 const Profile = document.querySelector('.profile')
-
-
+const everydayWordCloseButton = document.querySelector('.everyday_day-word-close-button');
+const everydayDateButton = document.querySelector('.everyday_date-container');
+const everydayDateCloseButton = document.querySelector('.everyday_date-close-button');
+const everydayNewWordButton = document.querySelector('.everyday_new-word-container');
+const everydayNewWordCloseButton = document.querySelector('.everyday_new-word-close-button');
+const dayHistoryButton = document.querySelector('.day_history');
+const newWordButton = document.querySelector('.new_word');
 function activeLink() {
   list.forEach((item) =>
     item.classList.remove('active'));
@@ -108,6 +114,32 @@ settingsButton.onclick = function () {
 }
 Profile.onclick = function () {
   audiomeme.play();
+}
+everydayWord.onclick = function () {
+	containerGrayBackground.style = 'display: block;';
+	everydayWordButton .style = 'display: block';
+}
+everydayWordCloseButton.onclick = function () {
+	containerGrayBackground.style = 'display: none;';
+	everydayWordButton .style = 'display: none';
+}
+
+dayHistoryButton.onclick = function () {
+	containerGrayBackground.style = 'display: block;';
+	everydayDateButton.style = 'display: block';
+}
+everydayDateCloseButton.onclick = function () {
+	containerGrayBackground.style = 'display: none;';
+	everydayDateButton.style = 'display: none';
+}
+
+newWordButton.onclick = function () {
+	containerGrayBackground.style = 'display: block;';
+	everydayNewWordButton.style = 'display: block';
+}
+everydayNewWordCloseButton.onclick = function () {
+	containerGrayBackground.style = 'display: none;';
+	everydayNewWordButton.style = 'display: none';
 }
 
 function getSum(a, b) {
