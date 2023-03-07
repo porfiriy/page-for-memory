7eebd4eb3df08efb1c5bcaf3b8fd852119694325
Aleksey <butor.aleksey@mail.ru>
2023-03-07 19:29:06 +0300

    Fix:(Menu) huge changes in settings

--- pages/settings-page/settings-page.js
+++ pages/settings-page/settings-page.js
@@ -2,14 +2,26 @@
 const Devs = document.querySelector('.developers-button');
 const DevsList = document.querySelector('.developers-container_body');
 const containerGrayBackground = document.querySelector('.gray-background-container');
-const DevsClose = document.querySelector('.close');
-
+const Close = document.querySelector('.close');
+const AboutUs = document.querySelector('.about-us-container');
+const AboutButton = document.querySelector('.about');
+const CloseAboutUs = document.querySelector('.close-about-us');
 Devs.onclick = function () { //при нажатии на кнопку
 	DevsList.style = 'display: block;';
 	containerGrayBackground.style = 'display: block;';
 }
 
-DevsClose.onclick = function () {
+Close.onclick = function () {
 	DevsList.style = 'display: none;';
+	containerGrayBackground.style = 'display: none;';
+}
+
+AboutButton.onclick = function () {
+	AboutUs.style = 'display: flex;';
+	containerGrayBackground.style = 'display: block;';
+}
+
+CloseAboutUs.onclick = function () {
+	AboutUs.style = 'display: none;';
 	containerGrayBackground.style = 'display: none;';
 }
