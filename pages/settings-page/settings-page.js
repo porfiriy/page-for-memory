26654060ccdec6c8c3bf87b0aeaab770612c4df1
Aleksey <butor.aleksey@mail.ru>
2023-03-07 20:53:28 +0300

    Fix:(Menu) little changes in settings

--- pages/settings-page/settings-page.js
+++ pages/settings-page/settings-page.js
@@ -6,22 +6,34 @@ const Close = document.querySelector('.close');
 const AboutUs = document.querySelector('.about-us-container');
 const AboutButton = document.querySelector('.about');
 const CloseAboutUs = document.querySelector('.close-about-us');
-Devs.onclick = function () { //при нажатии на кнопку
-	DevsList.style = 'display: block;';
+const LanguageButton = document.querySelector('.language-button');
+const Language = document.querySelector('.language-container');
+const CloseLanguage = document.querySelector('.close-language');
+
+Devs.onclick = function () { //Разработчики - при нажатии на кнопку
+	DevsList.style = 'display: flex;';
 	containerGrayBackground.style = 'display: block;';
 }
 
-Close.onclick = function () {
+Close.onclick = function () { //Разработчики - при закрытии
 	DevsList.style = 'display: none;';
 	containerGrayBackground.style = 'display: none;';
 }
 
-AboutButton.onclick = function () {
+AboutButton.onclick = function () { //О нас - при нажатии на кнопку
 	AboutUs.style = 'display: flex;';
 	containerGrayBackground.style = 'display: block;';
 }
 
-CloseAboutUs.onclick = function () {
+CloseAboutUs.onclick = function () { //О нас - при закрытии
 	AboutUs.style = 'display: none;';
+	containerGrayBackground.style = 'display: none;';
+}
+LanguageButton.onclick = function () { //Язык - при нажатии на кнопку
+	Language.style = 'display: flex;';
+	containerGrayBackground.style = 'display: block;';
+}
+CloseLanguage.onclick = function () { //Язык - при закрытии
+	Language.style = 'display: none;';
 	containerGrayBackground.style = 'display: none;';
 }
